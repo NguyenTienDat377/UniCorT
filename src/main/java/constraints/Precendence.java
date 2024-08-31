0fb3c05bffff965aa02302502d0813dc795c93e0
Nguyễn Tiến Đạt <112878795+NguyenTienDat377@users.noreply.github.com>
2024-08-31 23:20:33 +0700

    Update entities and a little bit of constraints

--- src/main/java/constraints/Precendence.java
+++
@@ -1,4 +0,0 @@
-package constraints;
-
-public class Precendence {
-}
--- src/main/java/constraints/utils/Utils.java
+++ src/main/java/constraints/utils/Utils.java
@@ -1,8 +1,10 @@
 package constraints.utils;
 
+import entities.Time;
+
 public class Utils {
     public static String andDay(Time t1, Time t2) {
-        
+        return "";
     }
 
 }
